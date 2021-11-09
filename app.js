22c7df497b0fa6183c01a37008424a5d362e9b98
JonathanEmanuel12 <jonathaanemanuel21@gmail.com>
2021-11-09 12:14:20 -0300

    pequena modificacao

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-var createError = require('http-errors');
 var express = require('express');
+var app = express();
+
+
+var createError = require('http-errors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -7,7 +10,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -38,4 +41,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+app.listen(3000, () => {
+    console.log("Rodando na porta 3000");
+})
